1141f867ebf38c7c1204f2189abe5d72c3860ee8
Glenn Hoppe <mail@ghoppe.ca>
2013-09-10 21:24:49 -0600

    don't register that

--- config/initializers/refinery/core.rb
+++ config/initializers/refinery/core.rb
@@ -1,4 +1,3 @@
 Refinery::Core.configure do |config|
 	config.register_stylesheet "refinerycms-bootstrap.css.scss", :media => 'screen' 
-	config.register_stylesheet "layout.css.scss", :media => 'screen' 
 end
