8649097ad1b05af51add5f5ea7690a3a1179342d
Glenn Hoppe <mail@ghoppe.ca>
2013-10-30 09:14:29 -0600

    Lots of changes for good Admin viewz

--- app/presenters/refinery/bootstrap/nav_presenter.rb
+++ app/presenters/refinery/bootstrap/nav_presenter.rb
@@ -1,22 +1,23 @@
 module Refinery
   module Bootstrap
-	class BootstrapMenuPresenter < ::Refinery::Pages::MenuPresenter
-	
-	config_accessor :collapse_at
-	self.collapse_at = -1
-	
-    self.menu_tag = :div
-	self.dom_id = nil
-    self.css = 'navbar'
-	self.selected_css = :active
-	
-	private
+    class NavPresenter < ::Refinery::Bootstrap::MenuPresenter
+
+    config_accessor :nav_style
+    self.nav_style = :pills
+    self.justified = false
+
+    self.css = 'nav'
+    self.css += ' nav-pills'    if nav_style.in?([:pills,:stacked])
+    self.css += ' nav-pills'    if nav_style.in?([:tabs])
+    self.css += ' nav-stacked'  if nav_style.in?([:stacked])
+    
+    self.selected_css = :active
+
+    private
       def render_menu(items)
-        content_tag(menu_tag, :id => dom_id, :class => css) do
-          content_tag(:div, render_menu_items(items, 'nav'), :class => 'navbar-inner')
-        end
+        content_tag(:ul, render_menu_items(items, 'nav'), :class => css)
       end
-      
+    
       def render_menu_items(menu_items, menu_css)
         if menu_items.present?
           content_tag(list_tag, :class => menu_css) do
@@ -27,29 +28,29 @@ module Refinery
           end
         end
       end
-      
+    
       def render_menu_item(menu_item, index)
         content_tag(list_item_tag, :class => menu_item_css(menu_item, index)) do
           buffer = ActiveSupport::SafeBuffer.new
           if menu_item_children(menu_item).present?
             buffer << link_to("#{menu_item.title} <b class=\"caret\"></b>", context.refinery.url_for(menu_item.url), :class => 'dropdown-toggle', 'data-toggle' => 'dropdown')
             buffer << render_menu_items(menu_item_children(menu_item), 'dropdown-menu')
-    	  else
+        else
             buffer << link_to(menu_item.title, context.refinery.url_for(menu_item.url))
-    	  end
+        end
           buffer
         end
       end
-      
+    
       def menu_item_css(menu_item, index)
         css = []
 
         css << selected_css if selected_item_or_descendant_item_selected?(menu_item)
         css << :dropdown if menu_item_children(menu_item).present?
-        
+      
         css.reject(&:blank?).presence
       end
 
-	end
+    end
   end
 end
---
+++ app/presenters/refinery/bootstrap/navbar_presenter.rb
@@ -0,0 +1,41 @@
+module Refinery
+  module Bootstrap
+		class NavbarPresenter < ::Refinery::Bootstrap::MenuPresenter
+			self.css = 'navbar navbar-default'
+			self.selected_css = :active
+			self.list_tag_css = 'nav navbar-nav'
+			self.first_css = nil
+			self.last_css  = nil
+
+			private
+				def render_navbar_header
+					content_tag(:div, :class => 'navbar-header') do
+						content_tag(:button, :class => 'navbar-toggle', :type => :button, :data => {:toggle => :collapse, :target => ".navbar-#{dom_id}-collapse"}) do
+							buffer = ActiveSupport::SafeBuffer.new
+							buffer << content_tag(:span,'Toggle navigation',  :class => 'sr-only')
+							3.times do
+								buffer << content_tag(:span, "", :class => 'icon-bar')
+							end
+							buffer
+						end
+					end			
+				end
+				
+				def render_collapse_menu_items(items)
+					content_tag(:div, :class => "collapse navbar-collapse navbar-#{dom_id}-collapse") do
+						render_menu_items(items)
+					end
+				end
+ 
+				def render_menu(items)
+					content_tag(menu_tag, :id => dom_id, :class => css) do
+						buffer = ActiveSupport::SafeBuffer.new
+						buffer << render_navbar_header
+						buffer << render_collapse_menu_items(items)
+						buffer
+					end
+				end
+
+		end
+  end
+end
