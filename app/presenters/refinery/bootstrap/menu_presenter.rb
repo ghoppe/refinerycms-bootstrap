b0fd6788e4700b63f9952dccb372d537e7294e7d
Glenn Hoppe <mail@ghoppe.ca>
2014-07-10 10:17:08 -0600

    fix dropdown menu class quote

--- app/presenters/refinery/bootstrap/menu_presenter.rb
+++ app/presenters/refinery/bootstrap/menu_presenter.rb
@@ -4,10 +4,10 @@ module Refinery
   module Bootstrap
     class MenuPresenter < ::Refinery::Pages::MenuPresenter
       include ActionView::Helpers::OutputSafetyHelper
-      
+
       # needed for refinerycms 2.1 stable. Only on master.
       config_accessor :list_tag_css
-      
+
       def render_menu_items(menu_items, menu_items_css)
         if menu_items.present?
           content_tag(list_tag, :class => menu_items_css) do
@@ -17,12 +17,12 @@ module Refinery
           end
         end
       end
-      
+
       def render_menu_item(menu_item, index)
         content_tag(list_item_tag, :class => menu_item_css(menu_item, index)) do
           buffer = ActiveSupport::SafeBuffer.new
           if menu_item_children(menu_item).present?
-            buffer << link_to(raw("#{menu_item.title} <b class=\"caret\"></b>"), context.refinery.url_for(menu_item.url), :class => 'dropdown-toggle', 'data-toggle' => 'dropdown')
+            buffer << link_to(raw(%q~#{menu_item.title} <b class="caret"></b>"~), context.refinery.url_for(menu_item.url), :class => 'dropdown-toggle', 'data-toggle' => 'dropdown')
             buffer << render_menu_items(menu_item_children(menu_item), 'dropdown-menu')
           else
             buffer << link_to(menu_item.title, context.refinery.url_for(menu_item.url))
@@ -30,7 +30,7 @@ module Refinery
           buffer
         end
       end
-      
+
     end
   end
 end
